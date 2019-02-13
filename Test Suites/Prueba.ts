2d85daed5eb8b63c50bee8ed09b7baac98f19c96
Nicolas Bermudes <nocicu@hotmail.com>
2019-02-13 15:43:24 -0500

    Primer commit v1.0

---
+++ Test Suites/Prueba.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Prueba</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ff8dc506-e775-49d8-ae4a-143dfcd1030d</testSuiteGuid>
+   <testCaseLink>
+      <guid>44e227d7-9557-4eb4-a749-4026c13d1fdc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Pruebakatalon</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
